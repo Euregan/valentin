ab9c210975c61dbf0625749b05bd07d8baa6d378
Valentin Manceaux-Panot <valentin@mcxp.io>
2024-03-26 15:31:42 +0100

    Fixed the base exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
-export { api, useMutation, useQuery } from "./next/legacy/api";
-export {
-  Error,
-  authentifiedGenerator,
-  handle,
-  visitor,
-} from "./next/legacy/endpoint";
+export { api, useMutation, useQuery } from "./next/api";
+export { Error, visitor } from "./next/endpoint";
 export { createFormHook, useForm } from "./form";
 export { default as jwt, JwtUser } from "./jwt";
 export { default as Result } from "./Result";
